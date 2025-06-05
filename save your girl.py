58fa160d162e34f7eebe8889f6f1ee80b007c012
RENATO AUGUSTO <63816078+Renato-Augusto0-68@users.noreply.github.com>
2025-06-05 20:19:18 -0300

    Update. fix of save your girl.py

--- save your girl.py
+++ save your girl.py
@@ -5,25 +5,15 @@ import random as r
 name=str(input("Write your name: ")).upper()
 print(f"Hello {name}, Welcome to : 'Save your love'.")
 print("Coded by Rutheford")
-k= r.uniform(0, 12)
-p= r.uniform(0, 12)
-print("Your girlfreind was kidnapped by an zombie. You have to kill the zombie to save her. Only has 3 attempts")
-print("The zombie's posiction is measured in a cartesian graphic")
-for g in range(1,4):
- x= int(input("x coordinate:  "))
- y=int(input("y coordinate:"))
- if x==k and y==p:
-    print(f"Congrats you  nailed it, on the {g} attempt. Now she is safe.")
- else:
-   print("You couldnt this time, try again")
-if x!=k and y!=p and g==3:
-  print("Game over, you failed.")print("Your girlfriend/boyfriend was kidnapped by an zombie. You have to kill the zombie to save her/him. You only has 3 attempts")
+k= r.uniform(0, 12) #The random selection of the coodinate X
+p= r.uniform(0, 12) #The random selection of the coodinate Y
+)print("Your girlfriend/boyfriend was kidnapped by an zombie. You have to kill the zombie to save her/him. You only has 3 attempts")
 print("The zombie's position is measured in a cartesian graphic")
 if name != 'BIANCA':
  for g in range(1,4):
-  x= float(input("x coordinate: "))
-  y= float(input("y coordinate: "))
-  if((x-k)+(y-p))==0.01:
+  x= float(input("x coordinate: ")) #The attempt of hitting the coodinate X
+  y= float(input("y coordinate: ")) #The attempt of hitting the coodinate Y
+  if((x-k)+(y-p))==0.01: #Limit of the difference between x and k (and respectivly) y and p, with that value gettin' near 0,01
     print(f"Congrats you nailed it, on the {g} attempt. Now she/he is safe. The coordinates were {k}, {p}.")
     break
   else:
@@ -39,7 +29,7 @@ if name != 'BIANCA':
    print(f"You missed this time, try again. The precision is {t}% on the x coordinate and {v}% on the y coordinate. If the percentage is higher than 100, it means that the value is above the zombie's position.")
  if ((x-k)+(y-p))!=0.01 and g==3:
   print(f"Game over,{name}, you failed. The coordinates were {k}, {p}. ")
-else:
+else: #the secret mode, that runs the same, but with more lifes
  for g in range(1,7):
    x= float(input("x coordinate: "))
    y= float(input("y coordinate: "))
